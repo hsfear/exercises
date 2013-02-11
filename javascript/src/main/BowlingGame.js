07043ec8699e5075954c3e70a52072f3e421daf3
Howard Fear <howard.fear@twcable.com>
2013-02-11 08:25:44 -0700

    Small bit of readability refactoring

--- javascript/src/main/BowlingGame.js
+++ javascript/src/main/BowlingGame.js
@@ -1,34 +1,39 @@
 var BowlingGame = (function() {
-    return function() {
-        var my = {
-            rolls: []
-        };
-
         var calculateScore = function(frame, rolls) {
             if (rolls.length === 0) return 0;
             if (rolls.length === 1) return rolls[0];
-            if (rolls[0] === 10) return sum(rolls.slice(0, 3)) + calculateScore(frame + 1, rolls.slice(1));
-            if (rolls[0] + rolls[1] === 10) return sum(rolls.slice(0, 3)) + calculateScore(frame + 1, rolls.slice(2));
+            if (hasStrike(rolls)) return sum(rolls.slice(0, 3)) + calculateScore(frame + 1, rolls.slice(1));
+            if (hasSpare(rolls)) return sum(rolls.slice(0, 3)) + calculateScore(frame + 1, rolls.slice(2));
             return rolls[0] + rolls[1] + calculateScore(frame + 1, rolls.slice(2));
-        }
+        };
 
         var checkDone = function(frame, rolls) {
             if (rolls.length === 0 || rolls.length === 1) return false;
-            if (frame === 10 && (rolls[0] === 10 || rolls[0] + rolls[1] === 10)) return rolls.length === 3;
-            if (frame === 10) return rolls.length === 2;
-            return  checkDone(frame + 1, rolls.slice(rolls[0] === 10 ? 1 : 2));
-        }
+            if (frame < 10) return checkDone(frame + 1, rolls.slice(hasStrike(rolls) ? 1 : 2));
+            return (hasStrike(rolls) || hasSpare(rolls)) ? rolls.length === 3 : rolls.length === 2;
+        };
+
+        var hasStrike = function(rolls) { return rolls.length >= 1 && rolls[0] === 10; };
+
+        var hasSpare = function(rolls) { return rolls.length >= 2 && rolls[0] + rolls[1] === 10; };
 
         var sum = function(numbers) {
             var sum = 0;
             for (var i = 0; i < numbers.length; i++)
                 sum += numbers[i];
             return sum;
-        }
+        };
 
-        this.score = function() { return calculateScore(1, my['rolls']); }
-        this.roll = function(roll) { my['rolls'].push(roll); return this; }
-        this.isDone = function() { return checkDone(1, my['rolls']); }
+        var DoneError = function() {};
+        DoneError.prototype = new Error();
+
+    return function() {
+        return {
+            rolls: [],
+            score: function() { return calculateScore(1, this.rolls); },
+            isDone: function() { return checkDone(1, this.rolls); },
+            roll: function(roll) { if (this.isDone()) throw new DoneError(); this.rolls.push(roll); return this; }
+        };
     };
 })();
                              
