8ff3029e51c9b35f8f0de35ce0dc3f78e94f1659
Howard Fear <howard.fear@twcable.com>
2013-02-15 19:03:49 -0800

    Move implementation method out of the function

--- javascript/src/main/BinarySearch.js
+++ javascript/src/main/BinarySearch.js
@@ -1,19 +1,20 @@
 module.exports = (function() {
-    return function() {
-        var chopImpl = function(value, array, start, end) {
-            var mid = start + Math.floor((end - start) / 2);
-            return (array[mid] === value) ? mid :
-                   (start === end) ? -1 :
-                   (array[mid] > value) ? chopImpl(value, array, start, (end !== mid) ? mid : start) :
-                   chopImpl(value, array, (start !== mid) ? mid : end, end);
-        };
+    var chopImpl = function(value, array, start, end) {
+        if (end - start <= 1)
+            return (value === array[start]) ? start :
+            (value === array[end]) ? end :
+            -1;
+        var mid = start + Math.floor((end - start) / 2);
+        return (array[mid] > value) ? chopImpl(value, array, start, mid) : chopImpl(value, array, mid, end);
+    };
 
+    return function() {
         return {
             chop: function(value, array) {
-                return (array.length ==  0) ? -1 :
-                       (value < array[0]) ? -1 :
-                       (value > array[array.length - 1]) ? -1 :
-                       chopImpl(value, array, 0, array.length - 1);
+                       return (array.length ==  0) ? -1 :
+                              (value < array[0]) ? -1 :
+                              (value > array[array.length - 1]) ? -1 :
+                              chopImpl(value, array, 0, array.length - 1);
             }
         };
     };
