605f8ef219ca6b1906fecfe3d11c993d2067329c
Howard Fear <howard.fear@twcable.com>
2013-12-26 19:42:10 -0700

    Simplify the alternate version of Exercise 1.7

--- sicp/1.7.2.scm
+++ sicp/1.7.2.scm
@@ -3,17 +3,17 @@
 (define (sqrt-iter guess change x)
         (if (good-enough? guess change)
             guess
-            (sqrt-iter (improve guess x) (- guess (improve guess x)) x)))
+            (sqrt-iter (+ guess change) (delta (+ guess change) x) x)))
 
 (define (square x) (* x x))
 
-(define (improve guess x) (average guess (/ x guess)))
+(define (delta guess x) (- (average guess (/ x guess)) guess))
 
 (define (average x y) (/ (+ x y) 2))
 
 (define (good-enough? guess change) (< (abs (/ change guess)) 0.001))
 
-(define (sqrt x) (sqrt-iter 1.0 x x))
+(define (sqrt x) (sqrt-iter 1.0 1.0 x))
 
 (sqrt 100)
 (sqrt 10000)
