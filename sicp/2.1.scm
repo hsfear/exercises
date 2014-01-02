d85a20cedfc64d484ad433b0f8c6484217f4bfd5
Howard Fear <howard.fear@twcable.com>
2014-01-02 08:26:44 -0700

    Exercise 2.1

---
+++ sicp/2.1.scm
@@ -0,0 +1,43 @@
+#lang scheme
+
+(define (gcd a b)
+  (if (= b 0)
+    a
+    (gcd b (remainder a b))))
+
+; (define (make-rat n d) (cons n d))
+(define (make-rat n d)
+  (if (< d 0) (make-rat (* -1 n) (* -1 d))
+    (let ((g (gcd (abs n) (abs d))))
+      (cons (/ n g) (/ d g)))))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (display (numer x))
+  (display "/")
+  (display (denom x))
+  (newline))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(print-rat (make-rat 1 2))
+(print-rat (make-rat -1 2))
+(print-rat (make-rat -1 -2))
+(print-rat (make-rat 1 -2))
