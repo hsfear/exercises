eb5be87d9e36403cebb97baa3e32b5946a0d3a0b
Howard Fear <howard.fear@twcable.com>
2014-01-04 11:14:42 -0500

    Exercise 2.7 refactor

--- sicp/2.7.scm
+++ sicp/2.7.scm
@@ -1,14 +1,8 @@
 #lang scheme
 
 (define (make-interval a b) (cons a b))
-(define (upper-bound interval)
-  (let ((a (car interval))
-        (b (cdr interval)))
-    (if (> a b) a b)))
-(define (lower-bound interval)
-  (let ((a (car interval))
-        (b (cdr interval)))
-    (if (< a b) a b)))
+(define (upper-bound interval) (max (car interval) (cdr interval)))
+(define (lower-bound interval) (min (car interval) (cdr interval)))
 
 (upper-bound (make-interval 3 8))
 (lower-bound (make-interval 3 8))
