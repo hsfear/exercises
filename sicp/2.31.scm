7c48eaec6ea02b6a59ce92b1ec996f360bcd3321
Howard Fear <howard.fear@twcable.com>
2014-01-06 09:49:13 -0500

    Exercise 2.31

---
+++ sicp/2.31.scm
@@ -0,0 +1,16 @@
+#lang scheme
+
+(define (tree-map p tree)
+   (map (lambda (sub-tree)
+           (if (pair? sub-tree)
+              (tree-map p sub-tree)
+               (p sub-tree)))
+         tree))
+
+(define (square x) (* x x))
+(define (square-tree tree) (tree-map square tree))
+
+(square-tree
+  (list 1
+        (list 2 (list 3 4) 5)
+        (list 6 7)))
