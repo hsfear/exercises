923468648cfe20b2aae6d9a922f1990c2e7de569
Howard Fear <howard.fear@twcable.com>
2014-01-07 20:05:55 -0700

    Exercise 2.46

---
+++ sicp/2.46.scm
@@ -0,0 +1,8 @@
+#lang scheme
+
+(define (make-vect x y) (cons x y))
+(define (xcor-vect v) (car v))
+(define (ycor-vect v) (cdr v))
+(define (add-vect v1 v2) (make-vect (+ (xcor-vect v1) (xcor-vect v2)) (+ (ycor-vect v1) (ycor-vect v2))))
+(define (sub-vect v1 v2) (make-vect (- (xcor-vect v1) (xcor-vect v2)) (- (ycor-vect v1) (ycor-vect v2))))
+(define (scale-vect factor v) (make-vect (* factor (xcor-vect v)) (* factor (ycor-vect v))))
