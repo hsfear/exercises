3ed70af466dfe2d9b75080e18d0787b41fcd88c1
Howard Fear <howard.fear@twcable.com>
2014-09-16 09:16:13 -0600

    copy ctor?

--- ruby/rosetta/oo/polymorphism/point.rb
+++ ruby/rosetta/oo/polymorphism/point.rb
@@ -1,5 +1,10 @@
 class Point
   attr_reader :x, :y
+  attr_writer :x, :y
+
+  def self.newCopy(original)
+      Point.new(original.x, original.y)
+  end
 
   def initialize(x, y)
     @x = x
--- ruby/rosetta/oo/polymorphism/point_spec.rb
+++ ruby/rosetta/oo/polymorphism/point_spec.rb
@@ -7,9 +7,25 @@ describe Point do
     expect(point.y).to eq(20)
   end
 
+  it "is can change x and y coordinates" do
+    point = Point.new(10, 20)
+    point.x = 30
+    point.y = 40
+    expect(point.x).to eq(30)
+    expect(point.y).to eq(40)
+  end
+
   it "prints the word Point" do
-    STDOUT.should_receive(:puts).with("Point")
+    expect(STDOUT).to receive(:puts).with("Point")
     point = Point.new(10, 20)
     point.print()
+  end
+
+  it "has a clone method that creates a new copy of the object" do
+    original = Point.new(10, 20)
+    clone = Point.newCopy(original)
+    original.x = 30
+    expect(clone.x).to eq(10)
+    expect(clone.y).to eq(20)
   end
 end
