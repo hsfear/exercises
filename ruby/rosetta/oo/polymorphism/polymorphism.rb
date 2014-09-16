ea0014fcf543dd3d11d6758a8ab39135f8f0c365
Howard Fear <howard.fear@twcable.com>
2014-09-16 11:00:23 -0600

    Consolidate exercise into single files

--- ruby/rosetta/oo/polymorphism/polymorphism.rb
+++ ruby/rosetta/oo/polymorphism/polymorphism.rb
@@ -1,4 +1,20 @@
-require_relative 'point'
+class Point
+  attr_reader :x, :y
+  attr_writer :x, :y
+
+  def self.newCopy(original)
+      Point.new(original.x, original.y)
+  end
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def print
+    puts("Point")
+  end
+end
 
 # Note: this is for the exercise - in the wild I would be more included to reverse the interitance or (better) have a circle have a point.
 class Circle < Point
--- ruby/rosetta/oo/polymorphism/polymorphism_spec.rb
+++ ruby/rosetta/oo/polymorphism/polymorphism_spec.rb
@@ -1,4 +1,34 @@
-require_relative 'circle'
+require_relative 'polymorphism'
+
+describe Point do
+  it "is created with x and y coordinates" do
+    point = Point.new(10, 20)
+    expect(point.x).to eq(10)
+    expect(point.y).to eq(20)
+  end
+
+  it "is can change x and y coordinates" do
+    point = Point.new(10, 20)
+    point.x = 30
+    point.y = 40
+    expect(point.x).to eq(30)
+    expect(point.y).to eq(40)
+  end
+
+  it "prints the word Point" do
+    expect(STDOUT).to receive(:puts).with("Point")
+    point = Point.new(10, 20)
+    point.print()
+  end
+
+  it "has a clone method that creates a new copy of the object" do
+    original = Point.new(10, 20)
+    clone = Point.newCopy(original)
+    original.x = 30
+    expect(clone.x).to eq(10)
+    expect(clone.y).to eq(20)
+  end
+end
 
 describe Circle do
   it "extends circle" do
