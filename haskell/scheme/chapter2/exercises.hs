3b9366d02ac682429755ec6e039e64d9fd4e198b
Howard Fear <howard.fear@twcable.com>
2015-11-22 18:26:28 -0500

    Chapter 2 exercise 1

---
+++ haskell/scheme/chapter2/exercises.hs
@@ -0,0 +1,52 @@
+module Main where
+import Control.Monad
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+main :: IO ()
+main = do args <- getArgs
+          putStrLn (readExpr (args !! 0))
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> "No Match: " ++ show err
+    Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = [first] ++ rest
+               return $ case atom of
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          otherwise -> Atom atom
+
+parseNumber :: Parser LispVal
+-- parseNumber = liftM (Number . read) $ many1 digit
+-- parseNumber = do digits <- many1 digit
+--                  return $ (Number . read) digits
+parseNumber = (many1 digit) >>= \digits -> return $ (Number . read) digits
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
