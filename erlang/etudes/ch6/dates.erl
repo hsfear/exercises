e7f6095a63eba9d2a81eb2f8bb0a39d4dc5fba7c
Howard Fear <howard.fear@twcable.com>
2015-12-06 20:23:54 -0700

    remove blank line

--- erlang/etudes/ch6/dates.erl
+++ erlang/etudes/ch6/dates.erl
@@ -1,7 +1,6 @@
 -module(dates).
 -export([date_parts/1,julian/1]).
 
-
 date_parts(String) -> [ to_integer(S) || S <- re:split(String,"-",[{return,list}]) ].
 
 to_integer(String) -> element(1, string:to_integer(String)).
