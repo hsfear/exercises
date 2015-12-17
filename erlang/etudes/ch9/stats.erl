19757bbe3a1ad752ba13d179a4d80454b2630cff
Howard Fear <howard.fear@twcable.com>
2015-12-16 19:58:53 -0700

    etude 9-1

---
+++ erlang/etudes/ch9/stats.erl
@@ -0,0 +1,43 @@
+-module(stats).
+-export([minimum/1,maximum/1,range/1,mean/1,stdv/1]).
+
+minimum([]) -> {error, badarg};
+minimum([Head|[]]) -> Head;
+minimum([Head|Tail]) ->
+    Min = minimum(Tail),
+    if
+        Head < Min -> Head;
+        true -> Min
+    end.
+
+maximum([]) -> {error, badarg};
+maximum([Head|[]]) -> Head;
+maximum([Head|Tail]) ->
+    Max = maximum(Tail),
+    if
+        Head > Max -> Head;
+        true -> Max
+    end.
+
+range([Head|Tail]) -> range(Tail, Head, Head).
+range([Head|Tail],Lower,Upper) when Head < Lower -> range(Tail, Head, Upper);
+range([Head|Tail],Lower,Upper) when Head > Upper -> range(Tail, Lower, Head);
+range([_|Tail],Lower,Upper) -> range(Tail, Lower, Upper);
+range([],Lower,Upper) -> [Lower, Upper].
+
+mean([]) -> {error, badarith};
+mean(Numbers) ->
+    try lists:foldl(fun(X, Sum) -> X + Sum end, 0, Numbers) / length(Numbers)
+    catch
+        error:_ -> {error, badarith}
+    end.
+
+
+stdv([]) -> {error, badarith};
+stdv(Numbers) ->
+    N = length(Numbers),
+    Sum = sum(Numbers),
+    SquaresSum = sum([ X * X || X <- Numbers]),
+    math:sqrt(((N * SquaresSum) - (Sum * Sum)) / (N * (N - 1))).
+
+sum(Numbers) -> lists:foldl(fun(X, Sum) -> X + Sum end, 0, Numbers).
