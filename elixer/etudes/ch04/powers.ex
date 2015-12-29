dbe7096a501138bc69893e551c23a71a4800505c
Howard Fear <howard.fear@twcable.com>
2015-12-28 21:48:51 -0700

    Etude 4-4

--- elixer/etudes/ch04/powers.ex
+++ elixer/etudes/ch04/powers.ex
@@ -1,7 +1,9 @@
 defmodule Powers do
-  import Kernel, except: [raise: 2]
+  import Kernel, except: [raise: 2, raise: 3]
 
   def raise(x, n) when n < 0, do: 1 / raise(x, abs(n))
-  def raise(_x, n) when n == 0, do: 1
-  def raise(x, n), do: x * raise(x, n - 1)
+  def raise(x, n), do: raise(x, n, 1)
+
+  defp raise(_x, n, acc) when n == 0, do: acc
+  defp raise(x, n, acc), do: raise(x, n - 1, x * acc)
 end
