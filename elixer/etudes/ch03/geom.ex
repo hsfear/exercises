1ff88b8ebe662e0fa4eeb35a674a650799f31f89
Howard Fear <howard.fear@twcable.com>
2015-12-28 17:43:00 -0700

    Etude 3-4

--- elixer/etudes/ch03/geom.ex
+++ elixer/etudes/ch03/geom.ex
@@ -1,14 +1,17 @@
 defmodule Geom do
-    def area(:rectangle, height, width) when height >= 0 and width >= 0 do
-        height * width
-    end
-    def area(:triangle, base, height) when base >= 0 and height >= 0 do
-        (base * height) / 2.0
-    end
-    def area(:ellipse, major, minor) when major >= 0 and major >= 0 do
-        :math.pi() * major * minor
-    end
-    def area(_, _, _) do
-        0
-    end
+  def area({shape, a, b}) do
+    area(shape, a, b)
+  end
+  defp area(:rectangle, height, width) when height >= 0 and width >= 0 do
+    height * width
+  end
+  defp area(:triangle, base, height) when base >= 0 and height >= 0 do
+    (base * height) / 2.0
+  end
+  defp area(:ellipse, major, minor) when major >= 0 and major >= 0 do
+    :math.pi() * major * minor
+  end
+  defp area(_, _, _) do
+    0
+  end
 end
