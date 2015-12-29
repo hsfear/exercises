0f5b81490c24340d4b5f696e79079d854a182626
Howard Fear <howard.fear@twcable.com>
2015-12-28 22:08:17 -0700

    Etude 4-5

--- elixer/etudes/ch04/powers.ex
+++ elixer/etudes/ch04/powers.ex
@@ -6,4 +6,21 @@ defmodule Powers do
 
   defp raise(_x, n, acc) when n == 0, do: acc
   defp raise(x, n, acc), do: raise(x, n - 1, x * acc)
+
+  def nth_root(x, n), do: nth_root(x, n, x / 2.0)
+  defp nth_root(x, n, a) do
+    IO.puts("Current guess is #{a}")
+    f = raise(a, n) - x
+    fprime = n * raise(a, n - 1)
+    next =  a - f / fprime
+    change = next - a
+
+    if abs(change) < 1.0e-8 do
+      next
+    else
+      nth_root(x, n, next)
+    end
+  end
+
+
 end
