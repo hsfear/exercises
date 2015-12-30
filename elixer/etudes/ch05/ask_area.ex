fea8f7ce24cc6ccdf6cf2de3f2626908bf404683
Howard Fear <howard.fear@twcable.com>
2015-12-29 20:14:31 -0700

    Etude 5-2

--- elixer/etudes/ch05/ask_area.ex
+++ elixer/etudes/ch05/ask_area.ex
@@ -4,6 +4,7 @@ defmodule AskArea do
     shape = char_to_shape(String.upcase(String.first(ans)))
     {a, b} = get_dimensions(shape)
     cond do
+      a == :error or b == :error -> IO.puts("Both entries must be numbers.")
       a < 0 or b < 0 -> IO.puts("Both numbers must be greater than or equal to zero.")
       shape == :unknown -> IO.puts("Unknown shape #{ans}")
       true -> Geom.area(shape, a, b)
@@ -16,14 +17,16 @@ defmodule AskArea do
   defp char_to_shape(_char), do: :unknown
 
   defp get_number(prompt) do
-    ans = IO.gets("Enter #{prompt} > ")
-    String.to_integer(String.strip(ans))
+    ans = String.strip(IO.gets("Enter #{prompt} > "))
+    cond do
+      Regex.match?(~r/\d+.\d+/, ans) -> String.to_float(ans)
+      Regex.match?(~r/\d+/, ans) -> String.to_integer(ans)
+      true -> :error
+    end
   end
   defp get_dimensions(prompt1, prompt2), do: { get_number(prompt1), get_number(prompt2) }
   defp get_dimensions(shape) when shape == :rectangle, do: get_dimensions("width", "height")
   defp get_dimensions(shape) when shape == :triangle, do: get_dimensions("base", "height")
   defp get_dimensions(shape) when shape == :ellipse, do: get_dimensions("major radius", "minor radius")
   defp get_dimensions(shape) when shape == :unknown, do: { 0, 0 }
-
-
 end
