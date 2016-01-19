6dacef1026ae2f8b446ad8d9093a482a325f8355
Howard Fear <howard.fear@twcable.com>
2016-01-19 08:26:33 -0700

    Day 1 exercise 2

--- lua/7lang/day1/easy.lua
+++ lua/7lang/day1/easy.lua
@@ -1,3 +1,19 @@
 function ends_in(num)
     return (math.abs(num) % 10) == 3
 end
+function is_prime(num)
+    if num < 2 then
+        return false
+    else
+        return check_prime(num, 2, num - 1)
+    end
+end
+function check_prime(num, start, last)
+    if start > last then
+        return true
+    elseif num % start == 0 then
+        return false
+    else
+        return check_prime(num, start + 1, math.floor(num / start))
+    end
+end
