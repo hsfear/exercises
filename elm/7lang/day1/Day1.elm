bf7ab421e7caa071535f2e3b35b45f2bb00a6001
Howard Fear <howard.fear@twcable.com>
2016-02-01 18:26:09 -0700

    Elm day1 remaining easy exercises

--- elm/7lang/day1/Day1.elm
+++ elm/7lang/day1/Day1.elm
@@ -1,5 +1,15 @@
 module Day1 where
 
-import List
+import List exposing (..)
 
-sum list = List.foldr (+) 0 list
+type alias Point = { x: Float, y: Float }
+type alias Address = { line1: String, line2: String, city: String, state: String, zipcode: String }
+type alias Person = { name: String, age: Int, address: Address }
+
+sum list = foldr (+) 0 list
+
+xs: List({x: Float, y: Float}) -> List(Float)
+xs list = map .x list
+
+point_xs: List(Point) -> List(Float)
+point_xs list = map .x list
--- elm/7lang/day1/Tests.elm
+++ elm/7lang/day1/Tests.elm
@@ -3,6 +3,7 @@ module Tests where
 import ElmTest exposing (..)
 
 import String
+import List exposing (..)
 
 import Day1
 
@@ -10,5 +11,7 @@ all : Test
 all =
     suite "Day1 Test Suite"
         [
-            test "Sum elements" (assertEqual (Day1.sum [1,2,3]) 6)
+            test "Sum elements" (assertEqual (Day1.sum [1,2,3]) 6),
+            test "record accessors " (assertEqual (Day1.xs [{x = 1, y=2}, {x =3, y=4}]) [1,3]),
+            test "Point accessors " (assertEqual (Day1.point_xs [{x = 1, y=2}, {x =3, y=4}]) [1,3])
         ]
