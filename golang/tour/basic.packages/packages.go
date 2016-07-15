c9bf2612303d33a6c233f930e11faae957f8dacb
Howard Fear <howard.fear@twcable.com>
2016-07-14 19:07:01 -0600

    Add some random numbers

--- golang/tour/basic.packages/packages.go
+++ golang/tour/basic.packages/packages.go
@@ -6,7 +6,12 @@ import (
 	"time"
 )
 
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	fmt.Printf("My favorite number is %d\n", rand.Intn(10))
+	fmt.Printf("My favorite addition is %d\n", rand.Intn(10)+rand.Intn(10))
 }
