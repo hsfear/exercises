f08527782c054bfc0149da61b0315d688135a5c9
Howard Fear <howard.fear@twcable.com>
2016-07-19 11:43:27 -0600

    Exercise 1.4

---
+++ golang/src/gobook/ch1/1.3/main/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string][]string)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, names := range counts {
+		if len(counts[line]) > 1 {
+			for _, name := range names {
+				fmt.Printf("%s\t%s\n", name, line)
+			}
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string][]string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()] = append(counts[input.Text()], f.Name())
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
+
+//!-
