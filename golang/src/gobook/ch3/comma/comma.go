68a0d47dd6cc32ccf35a5aaa9fb08064c4a5fce4
Howard Fear <howard.fear@twcable.com>
2016-07-25 06:50:58 -0600

    Handle optional sign

--- golang/src/gobook/ch3/comma/comma.go
+++ golang/src/gobook/ch3/comma/comma.go
@@ -9,19 +9,19 @@ import (
 // comma inserts commas in a non-negative decimal integer string.
 func Comma(s string) string {
 	var buf bytes.Buffer
-	n := len(s)
-	start := 0
-	if strings.HasPrefix(s, "-") {
+	if strings.HasPrefix(s, "-") || strings.HasPrefix(s, "+") {
 		buf.WriteString(s[0:1])
-		start = 1
+		s = s[1:]
 	}
-	initial := (n - start) % 3
+
+	n := len(s)
+	initial := n % 3
 	sep := ""
 	if initial > 0 {
-		buf.WriteString(s[start : start+initial])
+		buf.WriteString(s[:initial])
 		sep = ","
 	}
-	for i := start + initial; i < n; i += 3 {
+	for i := initial; i < n; i += 3 {
 		buf.WriteString(sep + s[i:(i+3)])
 		sep = ","
 	}
