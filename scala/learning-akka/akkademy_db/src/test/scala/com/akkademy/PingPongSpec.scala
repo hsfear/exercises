e7c47e0ba2f41f548b47d045138f4efdebb89788
Howard Fear <howard.fear@twcable.com>
2016-08-08 15:09:48 -0600

    Chapter 2 Ping example

--- scala/learning-akka/akkademy_db/src/test/scala/com/akkademy/PingPongSpec.scala
+++ scala/learning-akka/akkademy_db/src/test/scala/com/akkademy/PingPongSpec.scala
@@ -1,9 +1,14 @@
 package com.akkademy
 
 import akka.actor.{ActorSystem, Props}
+import akka.pattern.ask
 import akka.testkit.{ImplicitSender, TestKit}
+import akka.util.Timeout
 import com.akkademy.messages.{Ping, Pong}
 import org.scalatest.{BeforeAndAfterAll, FunSpecLike, Matchers}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 class PingPongSpec extends TestKit(ActorSystem("MySpec"))
   with ImplicitSender with FunSpecLike with Matchers with BeforeAndAfterAll
@@ -13,10 +18,20 @@ class PingPongSpec extends TestKit(ActorSystem("MySpec"))
   }
 
   describe("Ping Actor") {
+    val pingActor = system.actorOf(Props.create(classOf[PingActor]))
+    implicit val timeout = Timeout(5 seconds)
+
     it("Returns a Pong when it receives a Ping") {
-      val pingActor = system.actorOf(Props.create(classOf[PingActor]))
-      pingActor ! Ping()
-      expectMsg(Pong())
+      val future = pingActor ? Ping()
+      val result = Await.result(future.mapTo[String], 1 second)
+      assert(result == "Pong")
+    }
+
+    it("should fail on unknown message") {
+      val future = pingActor ? "unknown"
+      intercept[Exception] {
+        Await.result(future.mapTo[String], 1 second)
+      }
     }
   }
 }
