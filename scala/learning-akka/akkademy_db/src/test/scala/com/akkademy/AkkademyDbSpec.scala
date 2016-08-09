04c56193fce9598ad95ab6afe66b269acbf870f5
Howard Fear <howard.fear@twcable.com>
2016-08-09 17:20:57 -0600

    Chapter 2 Add GetRequest test

--- scala/learning-akka/akkademy_db/src/test/scala/com/akkademy/AkkademyDbSpec.scala
+++ scala/learning-akka/akkademy_db/src/test/scala/com/akkademy/AkkademyDbSpec.scala
@@ -1,23 +1,42 @@
 package com.akkademy
 
-import org.scalatest._
 import akka.actor.ActorSystem
-import com.akkademy.messages.{SetIfNotExists, SetRequest}
-import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import akka.pattern.ask
+import akka.testkit.{TestActorRef, TestKit}
 import akka.util.Timeout
+import com.akkademy.messages.{GetRequest, KeyNotFoundException, SetIfNotExists, SetRequest}
+import org.scalatest._
 
+import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class AkkademyDbSpec(_system: ActorSystem) extends TestKit(_system)
-  with FunSpecLike with Matchers with BeforeAndAfterAll with ImplicitSender {
-
-  def this() = this(ActorSystem("AkkademyDbSpec"))
+class AkkademyDbSpec extends FunSpecLike with Matchers with BeforeAndAfterAll {
+  implicit val system = ActorSystem()
+  implicit val timeout = Timeout(5 seconds)
 
   override def afterAll {
     TestKit.shutdownActorSystem(system)
   }
 
   describe("akkademyDb") {
+    describe("GetRequest") {
+      it("should return the value that was set for the key") {
+        val actorRef = TestActorRef(new AkkademyDb)
+        actorRef ! SetRequest("key", "value")
+        val futureResult = (actorRef ? GetRequest("key")).mapTo[String]
+        val value = Await.result(futureResult, 10 seconds)
+        value should equal("value")
+      }
+
+      it("should throw an exception if the key has not been set"){
+        val actorRef = TestActorRef(new AkkademyDb)
+        intercept[KeyNotFoundException] {
+          val futureResult = (actorRef ? GetRequest("key")).mapTo[String]
+          Await.result(futureResult, 10 seconds)
+        }
+      }
+    }
+
     describe("SetRequest") {
       it("should place key/value into map"){
         val actorRef = TestActorRef(new AkkademyDb)
