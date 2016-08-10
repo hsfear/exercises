8832145ddd68bbfb848923e123734558eddd445f
Howard Fear <howard.fear@twcable.com>
2016-08-09 18:02:57 -0600

    Chapter 2 Refactor to simplify test

--- scala/learning-akka/akkademy_db/src/test/scala/com/akkademy/AkkademyDbSpec.scala
+++ scala/learning-akka/akkademy_db/src/test/scala/com/akkademy/AkkademyDbSpec.scala
@@ -10,18 +10,20 @@ import org.scalatest._
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class AkkademyDbSpec extends FunSpecLike with Matchers with BeforeAndAfterAll {
-  implicit val system = ActorSystem()
+class AkkademyDbSpec extends FunSpecLike with Matchers with OneInstancePerTest with BeforeAndAfterAll {
+  implicit val system = AkkademyDbSpec.system
   implicit val timeout = Timeout(5 seconds)
 
   override def afterAll {
-    TestKit.shutdownActorSystem(system)
+    TestKit.shutdownActorSystem(AkkademyDbSpec.system)
   }
+
+  val actorRef = TestActorRef(new AkkademyDb)
+  val akkademyDb = actorRef.underlyingActor
 
   describe("akkademyDb") {
     describe("GetRequest") {
       it("should return the value that was set for the key") {
-        val actorRef = TestActorRef(new AkkademyDb)
         actorRef ! SetRequest("key", "value")
         val futureResult = (actorRef ? GetRequest("key")).mapTo[String]
         val value = Await.result(futureResult, 10 seconds)
@@ -29,7 +31,6 @@ class AkkademyDbSpec extends FunSpecLike with Matchers with BeforeAndAfterAll {
       }
 
       it("should throw an exception if the key has not been set"){
-        val actorRef = TestActorRef(new AkkademyDb)
         intercept[KeyNotFoundException] {
           val futureResult = (actorRef ? GetRequest("key")).mapTo[String]
           Await.result(futureResult, 10 seconds)
@@ -39,52 +40,39 @@ class AkkademyDbSpec extends FunSpecLike with Matchers with BeforeAndAfterAll {
 
     describe("SetRequest") {
       it("should place key/value into map"){
-        val actorRef = TestActorRef(new AkkademyDb)
         actorRef ! SetRequest("key", "value")
-
-        val akkademyDb = actorRef.underlyingActor
         akkademyDb.map.get("key") should equal(Some("value"))
       }
     }
 
     describe("SetIfNotExists") {
       it("should place the key/value into the map if the key does not already exist") {
-        val actorRef = TestActorRef(new AkkademyDb)
         actorRef ! SetIfNotExists("key", "value")
-
-        val akkademyDb = actorRef.underlyingActor
         akkademyDb.map.get("key") should equal(Some("value"))
       }
 
       it("should not update the key/value if the key already exists") {
-        val actorRef = TestActorRef(new AkkademyDb)
         actorRef ! SetIfNotExists("key", "value")
         actorRef ! SetIfNotExists("key", "new value")
-
-        val akkademyDb = actorRef.underlyingActor
         akkademyDb.map.get("key") should equal(Some("value"))
       }
     }
 
     describe("Delete") {
       it("should delete the key/value if it exists") {
-        val actorRef = TestActorRef(new AkkademyDb)
-
         actorRef ! SetRequest("key", "value")
         actorRef ! Delete("key")
-
-        val akkademyDb = actorRef.underlyingActor
         akkademyDb.map.get("key") should equal(None)
       }
 
       it("should succeed even it the key/value doesn't exists") {
-        val actorRef = TestActorRef(new AkkademyDb)
-
         actorRef ! Delete("key")
-
-        val akkademyDb = actorRef.underlyingActor
         akkademyDb.map.get("key") should equal(None)
       }
     }
   }
+}
+
+object AkkademyDbSpec {
+  val system = ActorSystem()
 }
