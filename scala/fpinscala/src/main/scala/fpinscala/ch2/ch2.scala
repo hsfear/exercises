23c7ce90e67198fb0cacd7beb859151b165b8307
Howard Fear <howard.fear@twcable.com>
2016-08-11 14:17:10 -0600

    Chapter 2 Remainder of exercises

--- scala/fpinscala/src/main/scala/fpinscala/ch2/ch2.scala
+++ scala/fpinscala/src/main/scala/fpinscala/ch2/ch2.scala
@@ -8,4 +8,9 @@ package object ch2 {
       else isSortedImpl(idx + 1)
     isSortedImpl(0)
   }
+
+  def partial1[A,B,C](a: A, f: (A,B) => C): B => C = (b: B) => f(a, b)
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = (a: A) => ((b:B) => f(a,b))
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a: A, b: B) => f(a)(b)
+  def compose[A,B,C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
 }
