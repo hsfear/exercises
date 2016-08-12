50cda6768adfc96ef99df530e30a0b236f21b704
Howard Fear <howard.fear@twcable.com>
2016-08-12 16:24:05 -0600

    Chapter 5 reimplemnt fibs and ones via unfold

--- scala/fpinscala/src/main/scala/fpinscala/ch5/ch5.scala
+++ scala/fpinscala/src/main/scala/fpinscala/ch5/ch5.scala
@@ -1,11 +1,10 @@
 package fpinscala
 
 package object ch5 {
-  def ones(): Stream[Int] = Stream.cons(1, ones())
-  def fibs(): Stream[Int] = {
-    def next(v1: Int, v2: Int): Stream[Int] = Stream.cons(v1, next(v2, v1 + v2))
-    next(0, 1)
-  }
+  def ones(): Stream[Int] = unfold(Nil)((_) => Some((1, Nil)))
+
+  def fibs(): Stream[Int] = unfold((0, 1)){ case (v1, v2) => Some((v1, (v2, v1 + v2))) }
+
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
     case None => Stream.empty
     case Some((v, s)) => Stream.cons(v, unfold(s)(f))
