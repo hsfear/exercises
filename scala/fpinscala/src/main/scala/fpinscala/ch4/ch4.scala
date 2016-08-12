5af08fdffb085477fed80f60ef711e8a56aed25c
Howard Fear <howard.fear@twcable.com>
2016-08-12 11:47:32 -0600

    Chapter 4 Rewrite map using a for comprehension

--- scala/fpinscala/src/main/scala/fpinscala/ch4/ch4.scala
+++ scala/fpinscala/src/main/scala/fpinscala/ch4/ch4.scala
@@ -2,10 +2,10 @@ package fpinscala
 
 package object ch4 {
 //  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a.flatMap(v1 => b.map(v2 => f(v1, v2)))
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
-    case (Some(v1), Some(v2)) => Some(f(v1, v2))
-    case (_, _) => None
-  }
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = for {
+    v1 <- a
+    v2 <- b
+  } yield f(v1, v2)
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = {
     def go[A](acc: List[A], options: List[Option[A]]): Option[List[A]] = options match {
