dd6b2d4554db5a4d8cdb52477795412b9fca91bf
Howard Fear <howard.fear@twcable.com>
2016-08-12 08:03:40 -0600

    Chapter 3 refactor size and depth as fold

--- scala/fpinscala/src/main/scala/fpinscala/ch3/Tree.scala
+++ scala/fpinscala/src/main/scala/fpinscala/ch3/Tree.scala
@@ -5,13 +5,12 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
-  def size[A](tree: Tree[A]): Int = tree match {
-    case Leaf(_) => 1
-    case Branch(left, right) => 1 + size(left) + size(right)
-  }
+  def size[A](tree: Tree[A]): Int = fold(tree)((_) => 1)((l, r) => 1 + l + r)
 
-  def depth[A](tree: Tree[A]): Int  = tree match {
-    case Leaf(_) => 1
-    case Branch(left, right) => 1 + (depth(left) max depth(right))
+  def depth[A](tree: Tree[A]): Int = fold(tree)((_) => 1)((l, r) => 1 + (l max r))
+
+  def fold[A,B](tree: Tree[A])(leaf: A => B)(branch: (B,B) => B): B =  tree match {
+    case Leaf(value) => leaf(value)
+    case Branch(left, right) => branch(fold(left)(leaf)(branch), fold(right)(leaf)(branch))
   }
 }
