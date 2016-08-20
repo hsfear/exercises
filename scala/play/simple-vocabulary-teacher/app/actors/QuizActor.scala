cb9393ad738470732366801c75979913128a80e4
Howard Fear <howard.fear@twcable.com>
2016-08-19 19:13:51 -0600

    Second half of chapter 4

---
+++ scala/play/simple-vocabulary-teacher/app/actors/QuizActor.scala
@@ -0,0 +1,34 @@
+package actors
+
+import akka.actor.{ Actor, ActorRef, Props }
+import play.api.i18n.Lang
+import services.VocabularyService
+
+class QuizActor(out: ActorRef, sourceLang: Lang, targetLang: Lang, vocabulary: VocabularyService) extends Actor {
+
+  private var word = ""
+
+  override def preStart(): Unit = sendWord()
+
+  def receive = {
+    case translation: String if vocabulary.verify(sourceLang, word, targetLang, translation) =>
+      out ! "Correct"
+      sendWord()
+    case _ => out ! "Incorrect, try again!"
+  }
+
+  def sendWord() = {
+    vocabulary.findRandomVocabulary(sourceLang, targetLang).map { v =>
+      out ! s"Please translate '${v.word}'"
+      word = v.word
+    } getOrElse {
+      out ! s"I don't know any word for ${sourceLang.code} and ${targetLang.code}"
+    }
+  }
+
+}
+
+object QuizActor {
+  def props(out: ActorRef, sourceLang: Lang, targetLang: Lang, vocabulary: VocabularyService): Props =
+    Props(classOf[QuizActor], out, sourceLang, targetLang, vocabulary)
+}
--- scala/play/simple-vocabulary-teacher/app/controllers/Quiz.scala
+++ scala/play/simple-vocabulary-teacher/app/controllers/Quiz.scala
@@ -2,9 +2,12 @@ package controllers
 
 import javax.inject.Inject
 
+import actors.QuizActor
 import play.api.i18n.Lang
-import play.api.mvc.{ Action, Controller }
+import play.api.mvc.{ Action, Controller, WebSocket }
 import services.VocabularyService
+
+import play.api.Play.current
 
 class Quiz @Inject() (vocabulary: VocabularyService) extends Controller {
   def quiz(sourceLanguage: Lang, targetLanguage: Lang) = Action {
@@ -15,10 +18,19 @@ class Quiz @Inject() (vocabulary: VocabularyService) extends Controller {
     }
   }
 
-  def check(sourceLanguage: Lang, word: String, targetLanguage: Lang, translation: String) = Action {
-    if (vocabulary.verify(sourceLanguage, word, targetLanguage, translation))
-      Ok
-    else
-      NotAcceptable
+  def check(sourceLanguage: Lang, word: String, targetLanguage: Lang, translation: String) = Action { request =>
+    val isCorrect = vocabulary.verify(sourceLanguage, word, targetLanguage, translation)
+    val correctScore: Int = request.session.get("correct").map(_.toInt).getOrElse(0)
+    val wrongScore = request.session.get("wrong").map(_.toInt).getOrElse(0)
+    if (isCorrect) {
+      Ok.withSession("correct" -> (correctScore + 1).toString, "wrong" -> wrongScore.toString)
+    } else {
+      NotAcceptable.withSession("correct" -> correctScore.toString, "wrong" -> (wrongScore + 1).toString)
+    }
   }
+
+  def quizEndpoint(sourceLang: Lang, targetLang: Lang) = WebSocket.acceptWithActor[String, String] { request =>
+    out => QuizActor.props(out, sourceLang, targetLang, vocabulary)
+  }
+
 }
