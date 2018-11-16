4342e1758c0b605fc47038d200ea108b28c27dca
Howard Fear <howard.fear@oracle.com>
2018-11-15 17:20:06 -0700

    Set up environment

---
+++ tdd/src/main/test/setup/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package setup;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+    @Test
+    void itShouldRun() {
+        Assertions.assertTrue(true);
+    }
+}
